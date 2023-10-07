b87e6c1a54d35ddd7c068c5b652da96f593f9b75
kintrix007 <boldizsar02@gmail.com>
2023-10-07 11:54:42 +0200

    format: Remove accidental extra indent

--- configuration.nix
+++ configuration.nix
@@ -296,7 +296,7 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 
   nixpkgs.overlays = [
