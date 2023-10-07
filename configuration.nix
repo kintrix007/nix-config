8a95935fa6edf93072e611cb29cfa2b47003d4d6
kintrix007 <boldizsar02@gmail.com>
2023-10-07 11:54:25 +0200

    feat: Setup ibus with mozc engine for Japanese input

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,27 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [
+      mozc
+    ];
+    # fcitx5.addons = with pkgs; [
+    #   fcitx5-mozc
+    #   fcitx5-gtk
+    # ];
+  };
+
+  # environment.sessionVariables =
+  #   let ime = "fcitx";
+  #   in {
+  #     INPUT_METHOD = ime;
+  #     QT_IM_MODULE = ime;
+  #     GTK_IM_MODULE = ime;
+  #     XMODIFIERS = "@im=${ime}";
+  #     XIM_SERVERS = ime;
+  #   };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
