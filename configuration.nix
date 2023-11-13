24bc7518c76e2395aa39cae072b032609cce7e71
kintrix007 <boldizsar02@gmail.com>
2023-11-13 17:55:48 +0100

    feat: Add correct setup to Avahi

--- configuration.nix
+++ configuration.nix
@@ -33,6 +33,20 @@
   networking.hostName = "yoshi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # networking.hosts = {
+  #   "192.168.178.122" = [ "rw1" ];
+  # };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    # publish = {
+    #   enable = true;
+    #   addresses = true;
+    #   domain = true;
+    # };
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
