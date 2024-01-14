863948664b9dd2b2af4570cd7a5cc758faecb86e
kintrix007 <kintrix007@proton.me>
2024-01-14 05:51:25 +0100

    Use the built-in way of enabling gsconnect
    Now no need to manually open ports

--- gnome/extensions.nix
+++ gnome/extensions.nix
@@ -1,18 +1,15 @@
 { config, pkgs, ... }:
 
-let
-  gs-connect-ports = pkgs.lib.range 1714 1764;
-in
 {
   environment.systemPackages = with pkgs.gnomeExtensions; [
     pop-shell
-    gsconnect
+    # gsconnect
     appindicator
     blur-my-shell
   ];
 
-  networking.firewall.allowedTCPPorts =
-    gs-connect-ports;
-  networking.firewall.allowedUDPPorts =
-    gs-connect-ports;
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
+  };
 }
