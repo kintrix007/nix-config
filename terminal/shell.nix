5cba711981a1790e61c5cca64eb9dad8fb05a038
kintrix007 <kintrix007@proton.me>
2024-02-23 21:29:35 +0100

    Update shell config

--- terminal/shell.nix
+++ terminal/shell.nix
@@ -1,26 +1,33 @@
 { config, pkgs, ... }:
 
 {
-    programs.bash = {
-      # enable = true; # Should not be explicitly enabled
-      blesh.enable = true;
-      enableCompletion = true;
-      enableLsColors = true;
-      undistractMe = {
-        enable = true;
-        timeout = 15;
-      };
-    };
-
-    programs.zsh = {
+  programs.bash = {
+    # enable = true; # Should not be explicitly enabled
+    blesh.enable = true;
+    enableCompletion = true;
+    enableLsColors = true;
+    undistractMe = {
       enable = true;
-      enableCompletion = true;
-      enableLsColors = true;
-      syntaxHighlighting = {
-        enable = true;
-        highlighters = [ "main" "brackets" ];
-      };
+      timeout = 15;
     };
+  };
 
-    users.defaultUserShell = pkgs.bash;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableLsColors = true;
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [ "main" "brackets" ];
+    };
+  };
+
+  users.defaultUserShell = pkgs.bash;
+
+  programs.direnv = {
+    enable = true;
+    silent = true;
+    loadInNixShell = true;
+    nix-direnv.enable = true;
+  };
 }
