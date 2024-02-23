e6713bde1e97b1798a5c7065af794e7539c7ff54
kintrix007 <kintrix007@proton.me>
2024-02-23 21:55:19 +0100

    Add games partition

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,12 @@
     }
   ];
 
+  fileSystems."/games" = {
+    device = "/dev/disk/by-uuid/fbdff21a-1844-4b94-b28a-59c1bc9cef8c";
+    fsType = "auto";
+    options = [ "defaults" "rw" "nofail" "user" ];
+  };
+
   networking.hostName = "yoshi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
