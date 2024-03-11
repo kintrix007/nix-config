d0a1b05924c9cc0b556d06642104c9aa27d9f05b
kintrix007 <kintrix007@proton.me>
2024-03-11 03:11:19 +0100

    Add all treesitter parsers that I could possibly want to use

--- editor/ts-parsers.nix
+++ editor/ts-parsers.nix
@@ -1,9 +1,46 @@
 { pkgs }:
 
 with pkgs.vimPlugins.nvim-treesitter-parsers; [
-    lua
-    c
-    rust
-    haskell
-    nix
+  lua
+  teal
+  c
+  cpp
+  rust
+  zig
+  haskell
+  agda
+  nix
+  python
+  bash
+  awk
+  typescript
+  javascript
+  tsx
+  janet_simple
+  julia
+  vim
+  vala
+  v
+  nim
+  sql
+  c_sharp
+
+  passwd
+  udev
+  ssh_config
+  diff
+  jq
+  cmake
+  make
+
+  yaml
+  json
+  jsonc
+  json5
+  toml
+  xml
+  doxygen
+  dockerfile
+  csv
+  dhall
 ]
