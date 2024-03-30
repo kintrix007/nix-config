fc89850ebef6c821490888afd2cee65e1389ad0f
kintrix007 <kintrix007@proton.me>
2024-03-30 13:26:29 +0100

    Switch to the vlc-bittorrent package in nixpkgs

--- vlc/default.nix
+++ vlc/default.nix
@@ -1,19 +1,12 @@
 { pkgs, config, ... }:
 
-let
-  vlc-bittorent = import ./vlc-bittorrent.nix { };
-in
 {
   environment.systemPackages = with pkgs; [
     vlc
   ];
 
   environment.sessionVariables = {
-    VLC_PLUGIN_PATH =
-      if pkgs.unstable ? vlc-bittorrent then
-        "${pkgs.unstable.vlc-bittorrent}"
-      else
-        "${vlc-bittorent}";
+    VLC_PLUGIN_PATH = "${pkgs.unstable.vlc-bittorrent}";
   };
 
   # For Chromecast support
