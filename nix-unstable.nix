a28d3c0dad8ce4e5819a145b67f886ca34856bb2
kintrix007 <kintrix007@proton.me>
2024-03-30 13:28:20 +0100

    Add note to unstable channel

--- nix-unstable.nix
+++ nix-unstable.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+# Note: It seems it will not even require an `--upgrade` for
+# it to fetch the latest unstable?
+
 let
   unstableChannel = fetchTarball "channel:nixpkgs-unstable";
 in
