5dc032c5cce8bdfcf5d5fe068621f0063aa0377d
kintrix007 <kintrix007@proton.me>
2024-05-02 10:42:46 +0200

    Add mangohud and protonup for niceties

--- steam.nix
+++ steam.nix
@@ -8,13 +8,22 @@
     gamescopeSession.enable = true;
   };
 
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/games/kin/proton";
+  };
+
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
     "steam"
     "steam-original"
     "steam-run"
   ];
 
-  programs.gamemode.enable = true;
-
-  # hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
+  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
 }
