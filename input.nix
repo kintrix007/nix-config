715b2d5d391a8e93b1d7d3a70c4472c32af70d54
kintrix007 <kintrix007@proton.me>
2024-05-04 10:13:36 +0200

    Add config for libinput mouse and touchpad

--- input.nix
+++ input.nix
@@ -22,9 +22,27 @@
   #     XIM_SERVERS = ime;
   #   };
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
+  };
+
+  services.xserver.libinput = {
+    enable = true;
+    touchpad = {
+      tapping = true; # Tap to click
+      disableWhileTyping = true;
+      # Bottom right for right click, bottom middle for middle click
+      clickMethod = "buttonareas";
+      # Two fingers for right click, three fingers for middle click
+      # clickMethod = "clickfinger";
+      naturalScrolling = true;
+      scrollMethod = "twofinger";
+    };
+    mouse = {
+      naturalScrolling = false;
+      middleEmulation = false; # Emulate middle click with left + right click
+      scrollButton = null;
+    };
   };
 }
