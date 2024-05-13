02adbd5fa770efd2b396910075887feb446459ac
kintrix007 <kintrix007@proton.me>
2024-05-13 10:20:33 +0200

    Fix out-of-sync press and release events on ibus

--- input.nix
+++ input.nix
@@ -1,30 +1,32 @@
 { pkgs, config, ... }:
 
+let
+  oldPkgs = import
+    (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/1b7a6a6e57661d7d4e0775658930059b77ce94a4.tar.gz";
+    })
+    { };
+in
 {
   i18n.inputMethod = {
     enabled = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [
+    # 1.5.28 has some major issues:
+    # https://github.com/ibus/ibus/issues/2480
+    # Downgrading to 1.5.27 fixes it
+    # Or alternatively, set IBUS_ENABLE_SYNC_MODE=1
+    ibus.engines = with oldPkgs.ibus-engines; [
       mozc
     ];
-    # fcitx5.addons = with pkgs; [
-    #   fcitx5-mozc
-    #   fcitx5-gtk
-    # ];
   };
 
-  # environment.sessionVariables =
-  #   let ime = "fcitx";
-  #   in {
-  #     INPUT_METHOD = ime;
-  #     QT_IM_MODULE = ime;
-  #     GTK_IM_MODULE = ime;
-  #     XMODIFIERS = "@im=${ime}";
-  #     XIM_SERVERS = ime;
-  #   };
+  environment.sessionVariables = {
+    IBUS_ENABLE_SYNC_MODE = "1";
+  };
 
-  services.xserver = {
+  services.xserver.xkb = {
+    options = "compose:rctrl,caps:swapescape";
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   services.xserver.libinput = {
