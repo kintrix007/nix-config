184216af00f16e12e749617a23bf40d50ef6abd5
kintrix007 <kintrix007@proton.me>
2024-05-21 23:35:33 +0200

    Switch to having a separate kmonad config file

--- input/kmonad/default.nix
+++ input/kmonad/default.nix
@@ -12,28 +12,12 @@
   services.kmonad = {
     enable = true;
     keyboards = {
-      builtinKeyboard = rec {
+      builtinKeyboard = {
         device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-        # config = builtins.readFile /path/to/my/config.kbd;
-        config = ''
-          (defcfg
-            input (device-file "${device}")
-            output (uinput-sink "My KMonad Output")
-
-            ;; Comment this if you want unhandled events not to be emitted
-            fallthrough true
-
-            ;; Set this to false to disable any command-execution in KMonad
-            allow-cmd false
-          )
-
-          (defsrc)
-
-          (deflayer layer1)
-        '';
+        config = builtins.readFile ./config.kbd;
       };
     };
 
-    # package = pkgs.haskellPackages.kmonad;
+    package = pkgs.haskellPackages.kmonad;
   };
 }
