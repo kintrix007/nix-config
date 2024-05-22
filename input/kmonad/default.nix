4064b5a5cc50e9b16dc4ad50662777e6cf19af9f
kintrix007 <kintrix007@proton.me>
2024-05-22 20:01:24 +0200

    Add gaming layer to kmonad

--- input/kmonad/default.nix
+++ input/kmonad/default.nix
@@ -15,6 +15,12 @@
       builtinKeyboard = {
         device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
         config = builtins.readFile ./config.kbd;
+
+        defcfg = {
+          enable = true;
+          allowCommands = false;
+          fallthrough = true;
+        };
       };
     };
 
