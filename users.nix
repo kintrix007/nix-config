f5fc144e83eb63fda4252ceb4484647f31d5712a
kintrix007 <kintrix007@proton.me>
2024-08-02 11:41:51 +0200

    Add stub itch user

--- users.nix
+++ users.nix
@@ -2,21 +2,31 @@
 
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.kin = {
-    isNormalUser = true;
-    description = "kin";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      # ! Do not add docker group. That is basically the same as root.
-      # "docker"
+  users.users = {
+    kin = {
+      isNormalUser = true;
+      description = "kin";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        # ! Do not add docker group. That is basically the same as root.
+        # "docker"
 
-      # For KMonad
-      "input"
-      "uinput"
-    ];
-    packages = with pkgs; [
-      gh
-    ];
+        # For KMonad
+        "input"
+        "uinput"
+      ];
+      packages = with pkgs; [
+        gh
+      ];
+    };
+
+    itch = {
+      isSystemUser = true;
+      password = null;
+      group = "itch";
+    };
   };
+
+  users.groups.itch = {};
 }
