eae9ec7589081b50dcc4c86f98e4ac45952c9a97
kintrix007 <kintrix007@proton.me>
2024-09-04 20:35:45 +0200

    Package DR Sprinto with Nix

--- hosts/goomba/drsprinto.nix
+++ hosts/goomba/drsprinto.nix
@@ -1,14 +1,21 @@
 { pkgs, lib, ... }:
 
+let
+  sprinto = (import ./sprinto { });
+in
 {
-  environment.systemPackages = with pkgs; [
-    lsb-release
-    vulkan-tools
+  # environment.systemPackages = with pkgs; [
+  #   lsb-release
+  #   vulkan-tools
+  # ];
+
+  environment.systemPackages = [
+    sprinto
   ];
 
-  # DR Sprinto has a GraphQL Server listen on this port
-  networking.firewall.allowedTCPPorts = [ 37370 ];
-  networking.firewall.allowedUDPPorts = [ 37370 ];
+  # DR Sprinto has a GraphQL Server listening on this port
+  # networking.firewall.allowedTCPPorts = [ 37370 ];
+  # networking.firewall.allowedUDPPorts = [ 37370 ];
   # ? Alternatively disable the firewall altogether.
   # networking.firewall.enable = lib.mkForce false;
 }
---
+++ hosts/goomba/sprinto/drsprinto.nix
@@ -0,0 +1,77 @@
+{ lib
+, lsb-release
+, stdenv
+, vulkan-tools
+, appimageTools
+, makeWrapper
+}:
+
+let
+  pname = "drsprinto";
+  version = "4.0.7";
+  src = /opt/drsprinto/DrSprinto-${version}.AppImage;
+
+  appimage = appimageTools.wrapType2 {
+    inherit pname version src;
+
+    extraPkgs = pkgs: with pkgs; [
+      lsb-release # Otherwise it fails to get system info
+
+      libGL
+      libglvnd
+      ffmpeg
+      amdvlk
+    ];
+  };
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version src;
+  };
+in
+
+# appimageContents
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = appimage;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildInputs = [
+    # lsb-release
+    # vulkan-tools # Maybe?
+  ];
+
+  strictDeps = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r bin $out/bin
+
+    mkdir -p $out/share/${pname}
+    cp -a ${appimageContents}/locales $out/share/${pname}
+    cp -a ${appimageContents}/resources $out/share/${pname}
+    cp -a ${appimageContents}/usr/share/icons $out/share/
+    install -Dm 644 ${appimageContents}/${pname}.desktop -t $out/share/applications
+
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace-fail "AppRun" "${pname}"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Security policy compliance software";
+    longDescription = ''
+      DrSprinto helps you keep your machine compatible with your organisation’s
+      security policies.
+    '';
+    homepage = "https://sprinto.com";
+    # license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ kintrix007 ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
