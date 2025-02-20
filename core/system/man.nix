895f576c7251777b6659e1ee746800f815ca245d
kintrix007 <kintrix007@proton.me>
2025-02-20 21:02:54 +0100

    Add more man pages. Still no man ld

--- core/system/man.nix
+++ core/system/man.nix
@@ -3,11 +3,13 @@
 {
   documentation.enable = true;
   documentation.man.enable = true;
+  # documentation.man.generateCaches = true; # Might be necessary?
   documentation.dev.enable = true;
 
   environment.systemPackages = with pkgs; [
     # Oh wow, didn't realize this is not default
     man-pages
+    man-pages-posix
     # linux-doc
     linux-manual
     linuxHeaders
