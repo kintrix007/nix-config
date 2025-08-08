371a440e31b47a249eca9e91a56dac3ebe78e091
kintrix007 <kintrix007@proton.me>
2025-08-08 19:25:18 +0200

    Update the /games drive UUID to refer to the new one

--- hosts/yoshi/fs.nix
+++ hosts/yoshi/fs.nix
@@ -8,8 +8,14 @@
     }
   ];
 
+  # Old broken drive
+  # fileSystems."/games" = {
+  #   device = "/dev/disk/by-uuid/fbdff21a-1844-4b94-b28a-59c1bc9cef8c";
+  #   fsType = lib.mkDefault "auto";
+  #   options = [ "defaults" "rw" "nofail" "user" "exec" ];
+  # };
   fileSystems."/games" = {
-    device = "/dev/disk/by-uuid/fbdff21a-1844-4b94-b28a-59c1bc9cef8c";
+    device = "/dev/disk/by-uuid/72c34b64-9cf3-403d-9707-f7bc1edc8ec3";
     fsType = lib.mkDefault "auto";
     options = [ "defaults" "rw" "nofail" "user" "exec" ];
   };
